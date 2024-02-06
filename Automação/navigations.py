5f4bad53959fc0f1f6c512874c444d2ea59df0a4
Eduardo Slomp Arán <105756006+ESAran@users.noreply.github.com>
2024-02-06 13:53:42 -0300

    Update navigations.py

--- Automação/navigations.py
+++ Automação/navigations.py
@@ -17,7 +17,7 @@ class Browser:
 
         - Faz a configuração do Browser para ser utilizado, abrindo o navegador através da passagem do caminho do chrome e chromedriver.
 
-        - Requere a passagem por parâmetro do site à ser aberto, opção de rodar em segundo plano com headless e a opção de abrir maximizado (padrão), através do maximized.
+        - Opção da passagem por parâmetro do site à ser aberto, opção de rodar em segundo plano com headless e a opção de abrir maximizado (padrão), através do maximized.
 
         - Retorna o ~driver~ para navegação.
 
@@ -37,7 +37,7 @@ class Browser:
         
         - Retorna o ~driver~ para navegação.
         '''
-    def chrome_browser(site, headless=False, maximized=True):
+    def chrome_browser(site='chrome://newtab', headless=False, maximized=True):
         '''
         chrome_browser():
 
